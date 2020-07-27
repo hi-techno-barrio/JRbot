6bdfbee5c6bc5d5c64b56f0155e78913698dc8df
Christopher Coballes <cobecoballes@gmail.com>
2020-07-27 19:56:10 +0800

    Update jrbot_controller.cpp

--- src/jrbot_controller.cpp
+++ src/jrbot_controller.cpp
@@ -1,9 +1,5 @@
-#include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Vector3.h>
-#include <stdio.h>
-#include <cmath>
+
+#include <include/rjbot.h>
 
 double radius = 0.04;                              //Wheel radius, in m
 double wheelbase = 0.187;                          //Wheelbase, in m
